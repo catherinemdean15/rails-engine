5ba74bb5b91048f2fbafdeb4f2673a4cc63174cf
Catherine Dean <catherinemdean15@gmail.com>
2021-02-14 13:41:43 -0700

    add create and strong params to items api controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,4 +10,14 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: Item.find(params[:id])
   end
+
+  def create
+    render json: Item.create!(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
