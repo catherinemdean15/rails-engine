2ff032c2b67717e9f23af5620772d332e74a56c1
Catherine Dean <catherinemdean15@gmail.com>
2021-02-13 17:03:43 -0700

    create routes for non-resftul merchant items and all items

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: %i[index show] do
-        resources :items, only: [:index]
+        get '/items', to: 'merchants#items'
       end
+      resources :items, only: [:index]
     end
   end
 end
