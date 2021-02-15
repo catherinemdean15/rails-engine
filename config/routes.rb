36d74c29ece5c2318b9e77896c603132921c01e7
Catherine Dean <catherinemdean15@gmail.com>
2021-02-15 11:11:25 -0700

    create route for most items action

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, only: %i[index show] do
         get '/items', to: 'merchants#items'
+        get '/merchants_items', to: 'merchants#merchants_items'
       end
       resources :items do
         get '/merchant', to: 'items#merchant'
       end
+      get '/most_items', to: 'merchants#most_items'
     end
   end
 end
