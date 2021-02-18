ee6be5bb28d61f5c64ac6188aa059722bde2f436
Catherine Dean <catherinemdean15@gmail.com>
2021-02-17 20:09:38 -0700

    ensure transaction has invoice id as a foreign key

---
+++ db/migrate/20210217230412_remove_invoice_id_from_transactions.rb
@@ -0,0 +1,5 @@
+class RemoveInvoiceIdFromTransactions < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :transactions, :invoice_id
+  end
+end
---
+++ db/migrate/20210217230505_add_invoices_to_transactions.rb
@@ -0,0 +1,5 @@
+class AddInvoicesToTransactions < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :transactions, :invoice, null: false, foreign_key: true
+  end
+end
