bc3f453bd81172ccd07f7169b282ae394fa15c4a
Catherine Dean <catherinemdean15@gmail.com>
2021-02-18 21:49:31 -0700

    update weekly revenue serilaizer to format date correctly

--- app/serializers/weekly_revenue_serializer.rb
+++ app/serializers/weekly_revenue_serializer.rb
@@ -1,4 +1,8 @@
 class WeeklyRevenueSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :week, :revenue
+  attributes :revenue
+
+  attribute :week do |week_as_date|
+    week_as_date.week.to_date
+  end
 end
